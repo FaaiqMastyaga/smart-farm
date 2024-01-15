acde0a06a468e62315a9ebe92e0c3bf5b4c8bed6
rakyankay <rakyankay@gmail.com>
2024-01-15 10:29:06 +0700

    fix: sidebar not running

--- js/script.js
+++ js/script.js
@@ -1,44 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const menuButton = document.getElementById("menu");
-    const plusCard = document.getElementById("plus-card");
-    const plantContainer = document.getElementById("plant-container");
+  const menuButton = document.getElementById("menu");
+  const plusCard = document.getElementById("plus-card");
+  const plantContainer = document.getElementById("plant-container");
 
-    menuButton.addEventListener("click", activateSideBar);
-    plusCard.addEventListener("click", addCard);
+  menuButton.addEventListener("click", activateSideBar);
+  plusCard.addEventListener("click", addCard);
 
-    plantContainer.addEventListener("click", (event) => {
-        const target = event.target;
-        if (target.classList.contains("delete-button") || target.closest(".delete-button")) {
-            deleteCard(target.closest(".card"));
-        } else if (target.classList.contains("card") || target.closest(".card")) {
-            activateCard(target.closest(".card"));
-        }
-    });
-})
+  plantContainer.addEventListener("click", (event) => {
+    const target = event.target;
+    if (
+      target.classList.contains("delete-button") ||
+      target.closest(".delete-button")
+    ) {
+      deleteCard(target.closest(".card"));
+    } else if (target.classList.contains("card") || target.closest(".card")) {
+      activateCard(target.closest(".card"));
+    }
+  });
+});
 
 function activateSideBar() {
-    const sidebar = document.querySelector('.sidebar');
-    const section = document.querySelector('.section');
-    sidebar.classList.toggle('active');
-    section.classList.toggle('shrink');
+  const sidebar = document.querySelector(".sidebar");
+  const section = document.querySelector(".section");
+  sidebar.classList.toggle("active");
+  section.classList.toggle("shrink");
 }
 
 function addCard() {
-    const cardContainer = document.getElementById("plant-container");
-    const plantName = prompt("Insert plant: ");
-    
-    if (plantName) {
-        const card = createCard(plantName);
-        cardContainer.insertBefore(card, cardContainer.lastElementChild);
-    }
+  const cardContainer = document.getElementById("plant-container");
+  const plantName = prompt("Insert plant: ");
+
+  if (plantName) {
+    const card = createCard(plantName);
+    cardContainer.insertBefore(card, cardContainer.lastElementChild);
+  }
 }
 
 function createCard(plantName) {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.setAttribute("id", "plant-card");
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.setAttribute("id", "plant-card");
 
-    card.innerHTML = `
+  card.innerHTML = `
         <div class="card-title">
             <div class="checked">
                 <i class="fa fa-circle"></i>
@@ -56,22 +59,23 @@ function createCard(plantName) {
         <div class="card-image">
 
         </div>
-    `    
-    return card;
+    `;
+  return card;
 }
 
 function deleteCard(card) {
-    const confirmation = confirm("Are you sure you want to delete this?");
+  const confirmation = confirm("Are you sure you want to delete this?");
 
-    if (confirmation) {
-        card.remove();
-    }
+  if (confirmation) {
+    card.remove();
+  }
 }
 
 function activateCard(card) {
-    const cards = document.querySelectorAll("#plant-card");
-    cards.forEach((c) => {
-        c.classList.remove("active");
-    });
-    card.classList.toggle("active");
+  const cards = document.querySelectorAll("#plant-card");
+  cards.forEach((c) => {
+    c.classList.remove("active");
+  });
+
+  card.classList.toggle("active");
 }

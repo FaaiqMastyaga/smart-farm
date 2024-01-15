b5bfc44f5c02f3623e21252762ba008d1cf3bd13
rakyankay <rakyankay@gmail.com>
2024-01-15 07:45:06 +0700

    feat: add card

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuButton = document.getElementById("menu");
+    const plusCard = document.getElementById("plus-card");
 
     menuButton.addEventListener("click", activateSideBar);
+    plusCard.addEventListener("click", addCard);
 })
 
 function activateSideBar() {
@@ -11,4 +13,10 @@ function activateSideBar() {
     section.classList.toggle('shrink');
 }
 
-fu
+function addCard() {
+    const cardContainer = document.querySelector(".card-container");
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.setAttribute("id", "plant-card");
+    cardContainer.insertBefore(card, cardContainer.lastElementChild);
+}

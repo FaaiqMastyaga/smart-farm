eef65736b72f633cd09b54dd3339b08300b33074
rakyankay <rakyankay@gmail.com>
2024-01-15 07:21:20 +0700

    fea: add footer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-export { activateSideBar };
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.getElementById("menu");
+
+    menuButton.addEventListener("click", activateSideBar);
+})
 
 function activateSideBar() {
     const sidebar = document.querySelector('.sidebar');
     const section = document.querySelector('.section');
-    document.querySelector('#menu').onclick = () => {
-        sidebar.classList.toggle('active');
-        section.classList.toggle('shrink');
-    }
+    sidebar.classList.toggle('active');
+    section.classList.toggle('shrink');
 }

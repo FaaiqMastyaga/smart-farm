190c7201d69e72082f75b196e7162c7d5544df7c
rakyankay <rakyankay@gmail.com>
2024-01-17 17:25:27 +0700

    refactor: separation of html and js code

--- js/monitor.js
+++ js/monitor.js
@@ -16,97 +16,44 @@ document.addEventListener("DOMContentLoaded", () => {
     displayCard();
 });
 
-function closeAddPlantContainer() {
-    const addPlantContainer = document.querySelector(".add-plant-container");
-    addPlantContainer.remove();
-    
-    const barrier = document.querySelector(".barrier");
-    barrier.remove();
-    
-    const content = document.querySelector(".wrapper");
-    content.classList.remove("dark");
-}
-
-function createBarrier() {
-    const body = document.querySelector("body");
-    const barrier = document.createElement("div");
-    barrier.classList.add("barrier");
-
-    const content = document.querySelector(".wrapper");
-    content.classList.add("dark");
-
-    body.appendChild(barrier);
-}
-
 function addPlant() {
-    const body = document.querySelector("body");
+    // display box to add plant
+    const addPlantBox = document.getElementById("add-plant-box");
+    addPlantBox.classList.add("active"); 
     
-    const addPlantContainer = document.createElement("div");
-    addPlantContainer.classList.add("add-plant-container");
-    
-    const formContainer = document.createElement("div");
-    formContainer.classList.add("form-container");
-    
-    const scheduleContainer = document.createElement("div");
-    scheduleContainer.classList.add("schedule-container");
-    
-    const formInput = document.createElement("div");
-    formInput.classList.add("form-input");
-    formInput.innerHTML = `
-        <label for="plant-name">Nama Tanaman:</label>
-        <input id="plant-name" type="text"> 
-        <br>
-        <label for="target-day">Target Panen:</label>
-        <input id="target-day" type="number"> 
-        <br>
-        <label for="schedule">Jadwal: </label>
-        <input 
-        id="schedule"
-        type="file" 
-            accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" 
-        > 
-        <br>
-    `;
-            
-    const formSelect = document.createElement("div");
-    formSelect.classList.add("form-select");
-    formSelect.innerHTML = `
-        <label for="plan-list">Nama Tanaman:</label>
-        <input list="plant-list">
-        <datalist id="plant-list">
-            <option value="Bayam">
-            <option value="Kangkung">
-            <option value="Jagung">
-            <option value="Singkong">
-            <option value="Cabai">
-            <option value="Sawi">
-        </datalist>
-        <br>
-    `;
-            
-    const scheduleDisplay = document.createElement("div");
-    scheduleDisplay.classList.add("schedule-display");
-
+    // create button inside form container
     const cancelButton = document.createElement("button");
     cancelButton.setAttribute("id", "cancel-button");
     cancelButton.textContent = "Cancel";
-    
-    formContainer.appendChild(formInput);
-    formContainer.appendChild(formSelect);
+    const formContainer = document.querySelector(".form-container");
     formContainer.appendChild(cancelButton); 
-    addPlantContainer.appendChild(formContainer);
-    addPlantContainer.appendChild(scheduleDisplay);
-    body.appendChild(addPlantContainer);
-    createBarrier();
-
-    cancelButton.addEventListener("click", closeAddPlantContainer);
+    
+    // activate form input tab
+    const formInput = document.getElementById("form-input");
+    formInput.classList.add("active");
+    
+    // activate form select tab
+    const formSelect = document.getElementById("form-select");
+    formSelect.classList.add("active"); // uncomment to activate
+    
+    // activate barrier -> penghalang supaya ga bisa ngeklik konten lain selain box untuk nambah tanaman
+    const barrier = document.querySelector(".barrier");
+    barrier.classList.add("active");
+    
+    // darken the content
+    const content = document.querySelector(".wrapper");
+    content.classList.add("dark");
+    
+    // exit add plant box
+    cancelButton.addEventListener("click", () => {
+        addPlantBox.classList.remove("active");
+        cancelButton.remove();
+        barrier.classList.remove("active");
+        content.classList.remove("dark");
+    });
 }
 
 function createCard(plantName, currentDay, remainingDay) {
-    if (!currentDay) {
-        currentDay = 0;
-    }
-
     const card = document.createElement("div");
     card.classList.add("card");
     card.setAttribute("id", "plant-card");
@@ -142,46 +89,31 @@ function createCard(plantName, currentDay, remainingDay) {
     return card;
 }
 
-function closeDeletePlantContainer() {
-    const addPlantContainer = document.querySelector(".delete-plant-container");
-    addPlantContainer.remove();
+function deletePlant(card) {
+    const deletePlantBox = document.getElementById("delete-plant-box");
+    deletePlantBox.classList.add("active");
 
     const barrier = document.querySelector(".barrier");
-    barrier.remove();
+    barrier.classList.add("active");
     
     const content = document.querySelector(".wrapper");
-    content.classList.remove("dark");
-}
-
-function deletePlant(card) {
-    const body = document.querySelector("body");
-
-    const deletePlantContainer = document.createElement("div");
-    deletePlantContainer.classList.add("delete-plant-container");
-    deletePlantContainer.textContent = "Delete";
-    deletePlantContainer.innerHTML = `
-        <div class="text-content">
-            <h2>Are you sure?</h2>
-            <br>
-            <p>This change cannot be undone. All values associated with this field will be lost.</p>
-        </div>
-        <div class="button-container">
-            <button class="button" id="delete-button">Delete</button>
-            <button class="button" id="cancel-button">Cancel</button>
-        </div>
-    `;
-
-    body.appendChild(deletePlantContainer);
-    createBarrier();
+    content.classList.add("dark");
 
     const deleteButton = document.getElementById("delete-button");
     const cancelButton = document.getElementById("cancel-button");
 
     deleteButton.addEventListener("click", () => {
         card.remove();
-        closeDeletePlantContainer();
+        deletePlantBox.classList.remove("active");
+        barrier.classList.remove("active");
+        content.classList.remove("dark");
     });
-    cancelButton.addEventListener("click", closeDeletePlantContainer)
+    
+    cancelButton.addEventListener("click", () => {
+        deletePlantBox.classList.remove("active");
+        barrier.classList.remove("active");
+        content.classList.remove("dark");
+    });
 }
 
 function activateCard(card) {
